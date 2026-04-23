4171b3b58fe650010a35bdcd8c950d43cc20a3ac
charansingh012-prog <charansingh012@gmail.com>
2026-04-23 16:26:31 +0530

    outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.ec2.id
+}
+
+output "public_ip" {
+  description = "EC2 Public IP"
+  value       = aws_instance.ec2.public_ip
+}
+
+output "private_ip" {
+  description = "EC2 Private IP"
+  value       = aws_instance.ec2.private_ip
+}
+
+output "instance_state" {
+  description = "EC2 Instance State"
+  value       = aws_instance.ec2.instance_state
+}
